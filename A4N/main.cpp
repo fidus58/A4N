697e759aad8f8694119f44973e413580883b5513
fidus58 <ahrens@informatik.hu-berlin.de>
2021-09-02 09:28:36 +0200

    Initial Commit

---
+++ A4N/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  A4N
+//
+//  Created by Klaus Ahrens on 02.09.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
